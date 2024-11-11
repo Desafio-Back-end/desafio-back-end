8f283a84ba24deeffbc3d771300860d5af599daf
Bruna Adam <brunaadam54@gmail.com>
2024-11-11 19:17:23 -0300

    Configuração Inicial / Implementação do BD

---
+++ prisma/migrations/20241111213258_primeira/migration.sql
@@ -0,0 +1,53 @@
+-- CreateTable
+CREATE TABLE "Usuario" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "nome" TEXT,
+    "tipoUsuario" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Instituicao" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "idUsuario" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Aluno" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "idUsuario" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Professor" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "idUsuario" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Disciplina" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "nome" TEXT,
+    "preRequisto" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Turma" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "idProfessor" INTEGER NOT NULL,
+    "idDisciplina" INTEGER NOT NULL,
+    "horarioTurno" TEXT,
+    "anoSemestre" TEXT,
+    "numVagas" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Matricula" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "idAluno" INTEGER NOT NULL,
+    "idTurma" INTEGER NOT NULL,
+    "status" TEXT
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Usuario_email_key" ON "Usuario"("email");
---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { InstituicoesModule } from './instituicoes/instituicoes.module';
+import { AlunosModule } from './alunos/alunos.module';
+import { ProfessoresModule } from './professores/professores.module';
+import { DisciplinasModule } from './disciplinas/disciplinas.module';
+import { MatriculasModule } from './matriculas/matriculas.module';
+import { TurmasModule } from './turmas/turmas.module';
+@Module({
+  imports: [ConfigModule.forRoot(), UsuariosModule, InstituicoesModule, AlunosModule, ProfessoresModule, DisciplinasModule, MatriculasModule, TurmasModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
